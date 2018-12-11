31180ee8065f9291841074947980a5dbf2c2ab22
Kyle Chung <kchung1068@jh-mba158-04.local>
2018-12-11 14:56:37 -0600

    Initial Commit

---
+++ The Ultimate Baldi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Ultimate Baldi
+//
+//  Created by Kyle Chung on 12/11/18.
+//  Copyright © 2018 Kyle Chung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
